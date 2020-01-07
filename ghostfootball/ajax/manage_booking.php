78251cc2c0bf9008517e6ca3c8a81dbb7686591a
Mohammad Z Shehadeh <MohammadZaidSh10@github.com>
2020-01-07 05:38:01 -0800

    T_T

--- ghostfootball/ajax/manage_booking.php
+++ ghostfootball/ajax/manage_booking.php
@@ -13,6 +13,8 @@ if (isset($_GET['durpick'])) {
 		$new = $start_time + $durpick;
 		if ($start_time == 11 && $durpick == 1) {
 			echo "<option value='12AM' selected>12:00 AM</option>";
+		}elseif($start_time == 10 &&  $durpick == 2 ){
+			echo "<option value='12AM' selected>12:00 AM</option>";
 		}elseif($start_time == 11 &&  $durpick == 2 ){
 			echo "<option value='1AM' selected>1:00 AM</option>";
 		}elseif($start_time == 12 && $durpick == 1){
@@ -28,6 +30,8 @@ if (isset($_GET['durpick'])) {
 		$durpick = $_GET['durpick'];
 		$new = $start_time + $durpick;
 		if ($start_time == 11 && $durpick == 1) {
+			echo "<option value='12PM' selected>12:00 PM</option>";
+		}elseif($start_time == 10 &&  $durpick == 2 ){
 			echo "<option value='12PM' selected>12:00 PM</option>";
 		}elseif($start_time == 11 &&  $durpick == 2 ){
 			echo "<option value='1PM' selected>1:00 PM</option>";
--- ghostfootball/ajax/search_booking.php
+++ ghostfootball/ajax/search_booking.php
@@ -4,7 +4,7 @@ include('../includes/connection.php');
 if (isset($_GET['search'])) {
 
     $query2  = "SELECT * FROM booking WHERE 
-    aside_id = {$_GET['aside_id']} AND booking_date='{$_GET['search']}'";
+    aside_id = {$_GET['aside_id']} AND booking_date='{$_GET['search']}' order by hour_start,hour_end ";
     $result2 = mysqli_query($conn,$query2);
 
     while ($row2 = mysqli_fetch_assoc($result2)) {
